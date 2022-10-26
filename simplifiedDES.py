de69b55db13ad144662ed3216e5abc3f83a5e11b
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-10-26 19:59:53 +0530

    added some reference pictures

---
+++ simplifiedDES.py
@@ -0,0 +1,10 @@
+# here i will write the code for simplified Data Encryption Algorithm
+# so there we have 3 main stages inside the these assignment and these are 
+# 1) Encryption 
+# 2) Key Generation
+# 3) Decryption
+
+# attribute of des algorithm 
+# plaintext : 8 bits
+# key : 10 bits
+# rounds : 2
